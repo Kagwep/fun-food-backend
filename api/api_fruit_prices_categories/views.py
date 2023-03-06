d3f0bc895cc899fde1a582a67a3a268fe2df1a8f
kagwe <kagwepeter07@gmail.com>
2023-03-06 15:13:57 +0300

    'db-update'

--- api/api_fruit_prices_categories/views.py
+++ api/api_fruit_prices_categories/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import serializers, viewsets
-from web.models import FruitssCategorie
+from web.models import PricessCategorie
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -11,11 +11,11 @@ from .permissions import UserPermission
 
 class PricesCategorySerializer(serializers.ModelSerializer):
     class Meta:
-        model = FruitssCategorie
+        model = PricessCategorie
         fields = ("id","p_name",)
         
 class PricesCategoryViewset(viewsets.ModelViewSet):
-    queryset = FruitssCategorie.objects.all()
+    queryset = PricessCategorie.objects.all()
     serializer_class = PricesCategorySerializer
     # authentication_classes = [JWTAuthentication]
     # permission_classes = (UserPermission,)
